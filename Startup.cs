5a478fa781e5574a0786e8a5ced65c51a2fe360e
Raju Ahmed <raju.ahmed@datasoft-bd.com>
2020-12-28 10:44:45 +0600

    test commit from develop

--- Startup.cs
+++ Startup.cs
@@ -34,7 +34,7 @@ namespace OdeToFood
 
             services.AddMvc().SetCompatibilityVersion(CompatibilityVersion.Version_2_1);
         }
-
+        //This a test commit.
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
         public void Configure(IApplicationBuilder app, IHostingEnvironment env)
         {
